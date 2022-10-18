2630c50a1cc61af4037c5e900b4031106ba95225
legacycodine <adebimpee842@gmail.com>
2022-10-17 17:25:29 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - inizialization of to print numbers
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num;
+
+	while (num < 10)
+	{
+		printf("%d", num);
+		num++;
+	}
+	printf('\n');
+	return (0);
+}
