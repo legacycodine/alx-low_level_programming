d7c7fcf74d2da97b1920957bd985cfcfe0df3a91
legacycodine <adebimpee842@gmail.com>
2022-10-17 17:56:27 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - initialization of alphabet code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(48 + num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
