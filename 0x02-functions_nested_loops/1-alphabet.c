8bea327d60ea6d61466327065ae022161279e834
legacycodine <adebimpee842@gmail.com>
2022-10-18 06:26:04 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,13 @@
  * Return: Always 0
  */
 
-void print_alphabet(void)
+int main(void)
 {
 	char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
 
-	_putchar('\n')
-		return (0);
-		
+	_putchar('\n');
+	return (0);
 }
