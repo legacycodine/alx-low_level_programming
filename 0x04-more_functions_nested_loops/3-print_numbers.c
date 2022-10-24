19f61798e4f03c223055b2f824399f71a05ec54b
legacycodine <adebimpee842@gmail.com>
2022-10-23 17:52:22 -0700

    number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - print 0 to 9
- * Return: Always 0
  */
 
 void print_numbers(void)
@@ -10,8 +9,7 @@ void print_numbers(void)
 	int a = 0;
 
 	while (a < 10)
-		_putchar(a++ + '0');
+		_putchar('0' + a);
 
 
 	_putchar('\n')
-}
