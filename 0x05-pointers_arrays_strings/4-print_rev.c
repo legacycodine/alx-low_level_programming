fcca47dd0340532e55179f375e4e9c95b314acb6
legacycodine <adebimpee842@gmail.com>
2022-10-24 14:10:55 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s - string to be printed
+ * Return: no return
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+
+	while (count > 0)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+	_putchar('\n');
+}
