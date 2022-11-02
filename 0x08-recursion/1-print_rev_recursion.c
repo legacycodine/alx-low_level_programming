22bd049b2f4e022b64c29e5c02c41810f66796a8
legacycodine <adebimpee842@gmail.com>
2022-11-02 05:45:49 -0700

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,16 +2,13 @@
 
 /**
  * _print_rev_recursion - prints a string in reverse
- * @s: parem string to be printed in reverse mode
+ * @s: string
  */
 
 void _print_rev_recursion(char *s)
 {
-
-		if (*s != '\0')
-		{
-			s++;
-			_print_rev_recursion(s);
-		}
-		_putchar(*s);
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
 }
