d62232c678671ece7363830f9e30fe118d07113a
legacycodine <adebimpee842@gmail.com>
2022-11-07 08:58:14 -0800

    CHESSBOARD

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_chessboard -0 print elements from an array of arrays
+ * @a: array to print
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row, column;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (column = 0; column < 8; column++)
+		{
+			_putchar(a[row][column]);
+		}
+		_putchar('\n');
+	}
+}
