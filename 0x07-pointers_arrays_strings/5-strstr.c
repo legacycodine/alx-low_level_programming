94af226c9f23a973bc4bce816f0fb0838f3fcd0b
legacycodine <adebimpee842@gmail.com>
2022-11-07 08:50:54 -0800

    STR

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - locate a substring in haystack string
+ * @haystack: string that may contain needle characters
+ * @needle: string that check against haystack
+ * Return: modified haystack if chars are found, NULL if not
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+
+	while (*haystack)
+	{
+		i = 0;
+		while (haystack[i] && needle[i] && haystack[i] == needle[i])
+			i++;
+		if (!needle[i])
+			return (haystack);
+		haystack++;
+	}
+	return (NULL);
+}
