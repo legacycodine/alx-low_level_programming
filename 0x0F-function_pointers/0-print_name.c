55dce70cb1569d38ba36b098aa2030fbf9894547
legacycodine <adebimpee842@gmail.com>
2022-11-18 04:57:53 -0800

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,22 @@
-#include "function_pointers.h"
 #include <stdlib.h>
+#include "function_pointers.h"
+
+
 
 /**
-   * print_name - prints name
-    * @name: string to be printed
-     * @f: callback function
-     */.
-
+  * print_name - prints a name
+  * @name: name to print
+  * @f: pointer to the printing function
+  */
 
 void print_name(char *name, void (*f)(char *))
+
 {
-	if (name != NULL || f != NULL)
-		f(name);
+
+	if (!name || !f)
+
+		return;
+
+	f(name);
 
 }
